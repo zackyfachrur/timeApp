c85f81a1f0c570d5cc63316828879244162bf497
zackyfachrur <mochzackyaja@gmail.com>
2024-04-22 22:48:19 +0700

    add login window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace DateApp
 {
     /// <summary>
@@ -18,52 +19,21 @@ namespace DateApp
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
-        public object RegisterName;
+
+        public static object RegisterName;
         public object RegisterEmail;
         public object RegisterPassword;
-        public object LoginName;
-        public object LoginEmail;
-        public object LoginPassword;
-        public object Login;
         public object register;
-
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            LoginGroup.Visibility = Visibility.Hidden;
-        }
-
-        public void RegisterData()
-        {
-            RegisterName = RegisterNameText.Text;
-            RegisterEmail = RegisterEmailText.Text;
-            RegisterPassword = RegisterPasswordText.Password;
-        }
-
-        public void LoginData()
-        {
-            LoginName = LoginNameText.Text;
-            LoginEmail = LoginEmailText.Text;
-            LoginPassword = LoginPasswordText.Password;
-        }
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            RegisterGroup.Visibility = Visibility.Hidden;
-            LoginGroup.Visibility = Visibility.Visible;
+            WindowLogin windowLogin = new WindowLogin();
+            this.Close();
+            windowLogin.Show();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (LoginName == RegisterName 
-                && LoginEmail == RegisterEmail 
-                && LoginPassword == RegisterPassword)
-            {
-                LoginGroup.Visibility = Visibility.Hidden;
-            }
-        }
 
     }
 }
---
+++ WindowLogin.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DateApp
+{
+    /// <summary>
+    /// Interaction logic for WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+        public WindowLogin()
+        {
+            this.InitializeComponent();
+            
+        }
+        public object LoginName;
+        public object LoginEmail;
+        public object LoginPassword;
+        public object Login;
+        public object RegisterName;
+        public object RegisterEmail;
+        public object RegisterPassword;
+        public object register;
+
+        public void LoginData()
+        {
+            LoginName = LoginNameText.Text;
+            LoginEmail = LoginEmailText.Text;
+            LoginPassword = LoginPasswordText.Password;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
